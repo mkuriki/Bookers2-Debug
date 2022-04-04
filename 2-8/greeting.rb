7711ef6aec5979a55409c07243fc468e337a1124
EC2 Default User <ec2-user@ip-172-31-22-121.ec2.internal>
2022-04-04 01:29:27 +0000

    [Start]2-8

---
+++ 2-8/greeting.rb
@@ -0,0 +1,12 @@
+def greeting(name)
+   return  "Hello, #{name}!"
+    "Good morning #{name}!"
+end
+
+puts greeting("John")
+
+def calc(num)
+    return "計算できません" if num.zero?
+    
+    ＃複雑な計算処理
+end
